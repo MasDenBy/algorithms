41d09dec4e3d3be121a04ed735e70bea2bad1fed
MasDenBy <horssupport@gmail.com>
2020-03-20 14:20:41 +0100

    The example of StringBuider

---
+++ Algorithms/Text/StringBuilder.cs
@@ -0,0 +1,57 @@
+﻿namespace Algorithms.Text
+{
+	/// <summary>
+	/// The example of realization StringBuilder.
+	/// </summary>
+	public class StringBuilder
+	{
+		private const int InitialLength = 50;
+
+		private char[] chars;
+		private int currentItemIndex;
+
+		public StringBuilder()
+			:this(InitialLength)
+		{ }
+
+		public StringBuilder(int length)
+		{
+			this.chars = new char[length];
+			this.currentItemIndex = 0;
+		}
+
+		public StringBuilder Append(string value)
+		{
+			if (string.IsNullOrWhiteSpace(value))
+				return this;
+
+			this.AllocateMoreSpaceIfNeeded(value.Length);
+
+			for (int i = 0; i < value.Length; i++)
+				this.chars[this.currentItemIndex++] = value[i];
+
+			return this;
+		}
+
+		public override string ToString()
+		{
+			return new string(this.chars, 0, this.currentItemIndex);
+		}
+
+		private void AllocateMoreSpaceIfNeeded(int need)
+		{
+			if (need < this.chars.Length - this.currentItemIndex)
+				return;
+
+			int newLength = this.chars.Length * 2;
+
+			while (need > newLength - this.currentItemIndex)
+				newLength *= 2;
+
+			var newCharacters = new char[newLength];
+			this.chars.CopyTo(newCharacters, 0);
+
+			this.chars = newCharacters;
+		}
+	}
+}
---
+++ AlgorithmsTest/Text/StringBuilderTest.cs
@@ -0,0 +1,47 @@
+﻿using Algorithms.Text;
+using NUnit.Framework;
+
+namespace AlgorithmsTest.Text
+{
+	[TestFixture]
+	public class StringBuilderTest
+	{
+		[Test]
+		public void AppendWithoutAllocationTest()
+		{
+			// Arrange
+			var sb = new StringBuilder();
+
+			// Act
+			sb
+				.Append("a")
+				.Append("bc")
+				.Append("  def ");
+
+			var text = sb.ToString();
+
+			// Assert
+			Assert.AreEqual("abc  def ", text);
+		}
+
+		[Test]
+		public void AppendWithAllocationTest()
+		{
+			// Arrange
+			var sb = new StringBuilder(20);
+
+			// Act
+			sb
+				.Append("Lorem ipsum dolor sit amet.")
+				.Append(" Quisque tincidunt finibus enim, quis vehicula enim posuere et.")
+				.Append(" Nam sagittis nibh a gravida consectetur.");
+
+			var text = sb.ToString();
+
+			// Assert
+			Assert.AreEqual(
+				"Lorem ipsum dolor sit amet. Quisque tincidunt finibus enim, quis vehicula enim posuere et. Nam sagittis nibh a gravida consectetur.", 
+				text);
+		}
+	}
+}
