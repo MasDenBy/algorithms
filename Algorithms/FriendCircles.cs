7ec09fcb8de1243d0154caa88dace5b61453e2d4
MasDenBy <horssupport@gmail.com>
2020-03-23 17:43:46 +0100

    Added FindUnion realization of finding friend's circles

---
+++ Algorithms/FriendCircles.cs
@@ -0,0 +1,84 @@
+﻿namespace Algorithms
+{
+	public class FriendCircles
+	{
+		public int FindCircleNum(int[][] array)
+		{
+			var uf = new UnionFind(array.Length);
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				for (int j = 0; j < array[i].Length; j++)
+				{
+					if (array[i][j] == 1)
+						uf.Union(i, j);
+				}
+			}
+
+			return uf.Count;
+		}
+
+		private class UnionFind
+		{
+			int[] unions;
+			int[] sizes;
+
+			public UnionFind(int unionsCount)
+			{
+				this.unions = new int[unionsCount];
+				this.sizes = new int[unionsCount];
+
+				for (int i = 0; i < unionsCount; i++)
+					this.unions[i] = i;
+			}
+
+			public int Count
+			{
+				get
+				{
+					int count = 0;
+					int[] temp = new int[this.unions.Length];
+
+					for (int i = 0; i < this.unions.Length; i++)
+					{
+						var root = GetRoot(i);
+						if (temp[root] == 0)
+							count++;
+
+						temp[root]++;
+					}
+
+					return count;
+				}
+			}
+
+			public void Union(int p, int q)
+			{
+				int i = GetRoot(p);
+				int j = GetRoot(q);
+
+				if (i == j)
+					return;
+
+				if(this.sizes[i] >= this.sizes[j])
+				{
+					this.unions[j] = i;
+					this.sizes[i]++;
+				} 
+				else
+				{
+					this.unions[i] = j;
+					this.sizes[j]++;
+				}
+			}
+
+			private int GetRoot(int i)
+			{
+				while (i != this.unions[i])
+					i = this.unions[i];
+
+				return i;
+			}
+		}
+	}
+}
---
+++ AlgorithmsTest/FriendCirclesTest.cs
@@ -0,0 +1,74 @@
+﻿using Algorithms;
+using NUnit.Framework;
+
+namespace AlgorithmsTest
+{
+	[TestFixture]
+	public class FriendCirclesTest
+	{
+		[Test]
+		public void FindCircleNumTwoGroupTest()
+		{
+			// Arrange
+			var m = new int[3][];
+			m[0] = new int[3] { 1, 1, 0 };
+			m[1] = new int[3] { 1, 1, 0 };
+			m[2] = new int[3] { 0, 0, 1 };
+
+			// Act
+			var count = new FriendCircles().FindCircleNum(m);
+
+			// Assert
+			Assert.AreEqual(2, count);
+		}
+
+		[Test]
+		public void FindCircleNumOneGroupTest()
+		{
+			// Arrange
+			var m = new int[3][];
+			m[0] = new int[3] { 1, 1, 0 };
+			m[1] = new int[3] { 1, 1, 1 };
+			m[2] = new int[3] { 0, 1, 1 };
+
+			// Act
+			var count = new FriendCircles().FindCircleNum(m);
+
+			// Assert
+			Assert.AreEqual(1, count);
+		}
+
+		[Test]
+		public void FindCircleNumThreeGroupTest()
+		{
+			// Arrange
+			var m = new int[3][];
+			m[0] = new int[3] { 1, 0, 0 };
+			m[1] = new int[3] { 0, 1, 0 };
+			m[2] = new int[3] { 0, 0, 1 };
+
+			// Act
+			var count = new FriendCircles().FindCircleNum(m);
+
+			// Assert
+			Assert.AreEqual(3, count);
+		}
+
+		[Test]
+		public void FindCircleNumOneGroupFourPeopleTest()
+		{
+			// Arrange
+			var m = new int[4][];
+			m[0] = new int[4] { 1, 0, 0, 1 };
+			m[1] = new int[4] { 0, 1, 1, 0 };
+			m[2] = new int[4] { 0, 1, 1, 1 };
+			m[3] = new int[4] { 1, 0, 1, 1 };
+
+			// Act
+			var count = new FriendCircles().FindCircleNum(m);
+
+			// Assert
+			Assert.AreEqual(1, count);
+		}
+	}
+}
