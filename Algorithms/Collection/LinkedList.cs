9bce02b702e6521de500782aa43386719705ac09
MasDenBy <horssupport@gmail.com>
2020-03-25 21:25:25 +0100

    Simple implementation of LinkedList

---
+++ Algorithms/Collection/LinkedList.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Collection
+{
+	public class LinkedList<T>
+	{
+		private Node<T> head;
+
+		public void AddFirst(T value)
+		{
+			var node = new Node<T>(value);
+
+			if (this.head == null)
+			{
+				this.head = node;
+			}
+			else
+			{
+				this.head.prev = node;
+				node.next = this.head;
+				this.head = node;
+			}
+		}
+
+		public void AddLast(T value)
+		{
+			var node = new Node<T>(value);
+
+			if (this.head == null)
+			{
+				this.head = node;
+			}
+			else
+			{
+				var lastNode = GetLastNode();
+				lastNode.next = node;
+				node.prev = lastNode;
+			}
+		}
+
+		public void RemoveFirst()
+		{
+			this.head = this.head.next != null
+				? this.head.next
+				: null;
+		}
+
+		public void RemoveLast()
+		{
+			var lastNode = this.GetLastNode();
+
+			if(lastNode.prev != null)
+			{
+				lastNode.prev.next = null;
+			}
+			else
+			{
+				lastNode = null;
+			}
+		}
+
+		public IEnumerable<T> GetElements()
+		{
+			var node = this.head;
+
+			while (node != null)
+			{
+				yield return node.element;
+
+				node = node.next;
+			}
+		}
+
+		private Node<T> GetLastNode()
+		{
+			var node = this.head;
+
+			while (node.next != null)
+				node = node.next;
+
+			return node;
+		}
+
+		private class Node<T>
+		{
+			public Node(T element)
+			{
+				this.element = element;
+			}
+
+			public T element;
+			public Node<T> next;
+			public Node<T> prev;
+		}
+	}
+}
---
+++ AlgorithmsTest/Collection/LinkedListTest.cs
@@ -0,0 +1,47 @@
+﻿using Algorithms.Collection;
+
+using NUnit.Framework;
+
+namespace AlgorithmsTest.Collection
+{
+	[TestFixture]
+	public class LinkedListTest
+	{
+		[Test]
+		public void AddTest()
+		{
+			// Arrange
+			var linkedList = new LinkedList<int>();
+
+			// Act
+			linkedList.AddFirst(2);
+			linkedList.AddLast(3);
+			linkedList.AddFirst(1);
+
+			var numbers = string.Join(',', linkedList.GetElements());
+
+			// Assert
+			Assert.AreEqual("1,2,3", numbers);
+		}
+
+		[Test]
+		public void RemoveTest()
+		{
+			// Arrange
+			var linkedList = new LinkedList<int>();
+			linkedList.AddLast(1);
+			linkedList.AddLast(2);
+			linkedList.AddLast(3);
+			linkedList.AddLast(4);
+
+			// Act
+			linkedList.RemoveFirst();
+			linkedList.RemoveLast();
+
+			var numbers = string.Join(',', linkedList.GetElements());
+
+			// Assert
+			Assert.AreEqual("2,3", numbers);
+		}
+	}
+}
