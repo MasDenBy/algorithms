263ad1e8b6e4fd60ef852031f483eeb859be8332
MasDenBy <horssupport@gmail.com>
2020-03-26 12:30:38 +0100

    Implementation of Queue

---
+++ Algorithms/Collection/Queue.cs
@@ -0,0 +1,67 @@
+﻿namespace Algorithms.Collection
+{
+	public class Queue<T>
+	{
+		private T[] values;
+		private int firstElementIndex;
+		private int lastElementIndex;
+
+		public Queue()
+			:this(2)
+		{ }
+
+		public Queue(int capacity)
+		{
+			this.values = new T[capacity];
+			this.firstElementIndex = 0;
+			this.lastElementIndex = -1;
+		}
+
+		private bool IsEmpty
+		{
+			get { return this.lastElementIndex < 0; }
+		}
+
+		public T Dequeue()
+		{
+			if (this.IsEmpty)
+				return default(T);
+
+			var element = this.values[this.firstElementIndex];
+			this.values[this.firstElementIndex] = default(T);
+			this.firstElementIndex++;
+
+			return element;
+		}
+
+		public void Enqueue(T value)
+		{
+			++this.lastElementIndex;
+
+			if (this.values.Length == this.lastElementIndex)
+				Resize(this.values.Length * 2);
+
+			this.values[this.lastElementIndex] = value;
+		}
+
+		public T Peek()
+		{
+			return this.IsEmpty
+				? default(T) 
+				: this.values[this.firstElementIndex];
+		}
+
+		private void Resize(int capacity)
+		{
+			var copy = new T[capacity];
+			var index = 0;
+
+			for (int i = this.firstElementIndex; i < this.lastElementIndex; i++)
+				copy[index++] = this.values[i];
+
+			this.firstElementIndex = 0;
+			this.lastElementIndex = index;
+			this.values = copy;
+		}
+	}
+}
---
+++ AlgorithmsTest/Collection/QueueTest.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using Algorithms.Collection;
+using NUnit.Framework;
+
+namespace AlgorithmsTest.Collection
+{
+	[TestFixture]
+	public class QueueTest
+	{
+		[Test]
+		public void DequeueIfEmptyShouldReturnDefaultTest()
+		{
+			// Arrange
+			var queue = new Queue<string>();
+
+			// Act
+			var result = queue.Dequeue();
+
+			// Assert
+			Assert.IsNull(result);
+		}
+
+		[Test]
+		public void EnqueueShouldAddElementTest()
+		{
+			// Arrange
+			const string test = "1";
+			var queue = new Queue<string>();
+
+			// Act
+			queue.Enqueue(test);
+
+			// Assert
+			Assert.That(queue.Peek(), Is.EqualTo(test));
+		}
+
+		[Test]
+		public void EnqueueAndDequeueTest()
+		{
+			// Arrange
+			const string test = "1";
+			var queue = new Queue<string>();
+
+			// Act
+			queue.Enqueue(test);
+			var actual = queue.Dequeue();
+
+			// Assert
+			Assert.That(actual, Is.EqualTo(test));
+			Assert.IsNull(queue.Peek());
+		}
+
+		[Test]
+		public void EnqueueWithResizeTest()
+		{
+			// Arrange
+			var queue = new Queue<string>();
+
+			// Act
+			queue.Enqueue("1");
+			queue.Enqueue("2");
+			queue.Dequeue();
+			queue.Enqueue("3");
+			queue.Enqueue("4");
+			queue.Enqueue("5");
+			queue.Dequeue();
+			queue.Enqueue("6");
+
+			var sb = new StringBuilder();
+			string data = null;
+			while ((data = queue.Dequeue()) != null)
+				sb.Append(data);
+
+			// Assert
+			Assert.That(sb.ToString(), Is.EqualTo("3456"));
+		}
+	}
+}
