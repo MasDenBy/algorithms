0a3c526e9d6939e81a94fd71c6742e54292d1ff0
MasDenBy <horssupport@gmail.com>
2020-03-26 10:00:33 +0100

    Convert Binary Number in a Linked List to Integer

---
+++ Algorithms/BynaryLinkedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+	public class BynaryLinkedList
+	{
+        public int GetDecimalValue(LinkedListNode<byte> head)
+        {
+            var sb = new StringBuilder();
+
+            var node = head;
+            while (node != null)
+            {
+                sb.Append(node.Value);
+                node = node.Next;
+            }
+
+            return Convert.ToInt32(sb.ToString(), 2);
+        }
+    }
+}
---
+++ AlgorithmsTest/BynaryLinkedListTest.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using Algorithms;
+using NUnit.Framework;
+
+namespace AlgorithmsTest
+{
+	[TestFixture]
+	public class BynaryLinkedListTest
+	{
+		[Test]
+		public void Test1()
+		{
+			// Arrange
+			var linkedList = new LinkedList<byte>();
+			linkedList.AddLast(1);
+			linkedList.AddLast(0);
+			linkedList.AddLast(1);
+
+			// Act & Assert
+			GetDecimalValueTest(linkedList, 5);
+		}
+
+		[Test]
+		public void Test2()
+		{
+			// Arrange
+			var linkedList = new LinkedList<byte>();
+			linkedList.AddLast(0);
+
+			// Act & Assert
+			GetDecimalValueTest(linkedList, 0);
+		}
+
+		[Test]
+		public void Test3()
+		{
+			// Arrange
+			var linkedList = new LinkedList<byte>();
+			linkedList.AddLast(1);
+
+			// Act & Assert
+			GetDecimalValueTest(linkedList, 1);
+		}
+
+		[Test]
+		public void Test4()
+		{
+			// Arrange
+			var linkedList = new LinkedList<byte>();
+			linkedList.AddLast(1);
+			linkedList.AddLast(0);
+			linkedList.AddLast(0);
+			linkedList.AddLast(1);
+			linkedList.AddLast(0);
+			linkedList.AddLast(0);
+			linkedList.AddLast(1);
+			linkedList.AddLast(1);
+			linkedList.AddLast(1);
+			linkedList.AddLast(0);
+			linkedList.AddLast(0);
+			linkedList.AddLast(0);
+			linkedList.AddLast(0);
+			linkedList.AddLast(0);
+			linkedList.AddLast(0);
+
+			// Act & Assert
+			GetDecimalValueTest(linkedList, 18880);
+		}
+
+		[Test]
+		public void Test5()
+		{
+			// Arrange
+			var linkedList = new LinkedList<byte>();
+			linkedList.AddLast(0);
+			linkedList.AddLast(0);
+
+			// Act & Assert
+			GetDecimalValueTest(linkedList, 0);
+		}
+
+		private static void GetDecimalValueTest(LinkedList<byte> linkedList, int expected)
+		{
+			// Act
+			var number = new BynaryLinkedList().GetDecimalValue(linkedList.First);
+
+			// Assert
+			Assert.That(number, Is.EqualTo(expected));
+		}
+	}
+}
