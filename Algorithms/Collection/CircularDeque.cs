a2e4cede690bf558f83bf22524c085c173d34364
MasDenBy <horssupport@gmail.com>
2020-03-27 20:21:01 +0100

    CircularDeque implementation with array

---
+++ Algorithms/Collection/CircularDeque.cs
@@ -0,0 +1,127 @@
+﻿namespace Algorithms.Collection
+{
+	public class CircularDeque
+	{
+		private const int DataMissedCode = -1;
+
+		private int[] values;
+
+		private int frontIndex;
+		private int lastIndex;
+
+		private int count;
+
+		public CircularDeque(int capacity)
+		{
+			this.values = new int[capacity];
+			this.frontIndex = -1;
+			this.lastIndex = -1;
+			this.count = 0;
+		}
+
+		public bool InsertFront(int value)
+		{
+			if (this.IsFull())
+				return false;
+
+			if(!InsertIntoEmptyDeque(value))
+			{
+				this.frontIndex = this.frontIndex == this.values.Length - 1
+					? 0
+					: this.frontIndex + 1;
+
+				this.values[this.frontIndex] = value;
+			}
+
+			this.count++;
+
+			return true;
+		}
+
+		public bool InsertLast(int value)
+		{
+			if (this.IsFull())
+				return false;
+
+			if (!InsertIntoEmptyDeque(value))
+			{
+				this.lastIndex = this.lastIndex == 0 
+					? this.values.Length - 1 
+					: this.lastIndex - 1;
+
+				this.values[this.lastIndex] = value;
+			}
+
+			this.count++;
+
+			return true;
+		}
+
+		public bool DeleteFront()
+		{
+			if (this.IsEmpty())
+				return false;
+
+			this.frontIndex = this.frontIndex == 0
+				? this.values.Length - 1
+				: this.frontIndex - 1;
+
+			this.count--;
+
+			return true;
+		}
+
+		public bool DeleteLast()
+		{
+			if (this.IsEmpty())
+				return false;
+
+			this.lastIndex = this.lastIndex == this.values.Length - 1 
+				? 0 
+				: this.lastIndex + 1;
+
+			this.count--;
+
+			return true;
+		}
+
+		public int GetFront()
+		{
+			if (this.IsEmpty())
+				return DataMissedCode;
+
+			return this.values[this.frontIndex];
+		}
+
+		public int GetRear()
+		{
+			if (this.IsEmpty())
+				return DataMissedCode;
+
+			return this.values[this.lastIndex];
+		}
+
+		public bool IsEmpty()
+		{
+			return this.count == 0;
+		}
+
+		public bool IsFull()
+		{
+			return this.count == this.values.Length;
+		}
+
+		private bool InsertIntoEmptyDeque(int value)
+		{
+			if (this.IsEmpty())
+			{
+				this.frontIndex = this.lastIndex = 0;
+				this.values[0] = value;
+
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ AlgorithmsTest/Collection/CircularDequeTest.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms.Collection;
+using NUnit.Framework;
+
+namespace AlgorithmsTest.Collection
+{
+	[TestFixture]
+	public class CircularDequeTest
+	{
+		[Test]
+		public void EmptyDequeTest()
+		{
+			// Arrange
+			var deque = new CircularDeque(3);
+
+			// Act & Assert
+			Assert.IsFalse(deque.DeleteFront());
+			Assert.IsFalse(deque.DeleteLast());
+			Assert.AreEqual(-1, deque.GetFront());
+			Assert.AreEqual(-1, deque.GetRear());
+			Assert.IsTrue(deque.IsEmpty());
+			Assert.IsFalse(deque.IsFull());
+		}
+
+		[Test]
+		public void Test1()
+		{
+			// Arrange
+			var deque = new CircularDeque(3);
+
+			// Act & Assert
+			Assert.IsTrue(deque.InsertLast(1));
+			Assert.IsTrue(deque.InsertLast(2));
+			Assert.IsTrue(deque.InsertFront(3));
+			Assert.IsFalse(deque.InsertFront(4));
+			Assert.AreEqual(2, deque.GetRear());
+			Assert.IsTrue(deque.IsFull());
+			Assert.IsTrue(deque.DeleteLast());
+			Assert.IsTrue(deque.InsertFront(4));
+			Assert.AreEqual(4, deque.GetFront());
+		}
+
+		[Test]
+		public void Test2()
+		{
+			// Arrange
+			var deque = new CircularDeque(3);
+
+			// Act & Assert
+			Assert.IsTrue(deque.InsertFront(2));
+			Assert.IsTrue(deque.InsertLast(4));
+			Assert.IsTrue(deque.InsertFront(6));
+			Assert.AreEqual(4, deque.GetRear());
+			Assert.IsFalse(deque.InsertFront(5));
+			Assert.AreEqual(6, deque.GetFront());
+			Assert.AreEqual(6, deque.GetFront());
+			Assert.IsFalse(deque.InsertFront(6));
+			Assert.IsTrue(deque.IsFull());
+			Assert.IsFalse(deque.InsertLast(6));
+			Assert.AreEqual(4, deque.GetRear());
+		}
+
+		[Test]
+		public void Test3()
+		{
+			// Arrange
+			var deque = new CircularDeque(3);
+
+			// Act & Assert
+			Assert.IsTrue(deque.InsertFront(9));
+			Assert.AreEqual(9, deque.GetRear());
+			Assert.IsTrue(deque.InsertFront(9));
+			Assert.AreEqual(9, deque.GetRear());
+			Assert.IsTrue(deque.InsertLast(5));
+			Assert.AreEqual(9, deque.GetFront());
+			Assert.AreEqual(5, deque.GetRear());
+			Assert.AreEqual(9, deque.GetFront());
+			Assert.IsFalse(deque.InsertLast(8));
+			Assert.IsTrue(deque.DeleteLast());
+			Assert.AreEqual(9, deque.GetFront());
+		}
+
+		[Test]
+		public void Test4()
+		{
+			// Arrange
+			var deque = new CircularDeque(3);
+
+			// Act & Assert
+			Assert.IsTrue(deque.InsertFront(8));
+			Assert.IsTrue(deque.InsertLast(8));
+			Assert.IsTrue(deque.InsertLast(2));
+			Assert.AreEqual(8, deque.GetFront());
+			Assert.IsTrue(deque.DeleteLast());
+			Assert.AreEqual(8, deque.GetRear());
+			Assert.IsTrue(deque.InsertFront(9));
+			Assert.IsTrue(deque.DeleteFront());
+			Assert.AreEqual(8, deque.GetRear());
+			Assert.IsTrue(deque.InsertLast(2));
+			Assert.IsTrue(deque.IsFull());
+		}
+
+		[Test]
+		public void Test5()
+		{
+			// Arrange
+			var deque = new CircularDeque(4);
+
+			// Act & Assert
+			Assert.IsTrue(deque.InsertFront(9));
+			Assert.IsTrue(deque.DeleteLast());
+			Assert.AreEqual(-1, deque.GetRear());
+			Assert.AreEqual(-1, deque.GetFront());
+			Assert.AreEqual(-1, deque.GetFront());
+			Assert.IsFalse(deque.DeleteFront());
+			Assert.IsTrue(deque.InsertFront(6));
+			Assert.IsTrue(deque.InsertLast(5));
+			Assert.IsTrue(deque.InsertFront(9));
+			Assert.AreEqual(9, deque.GetFront());
+			Assert.IsTrue(deque.InsertFront(6));
+		}
+
+		[Test]
+		public void FrontPointTheSameElementAsRearTest()
+		{
+			// Arrange
+			var deque = new CircularDeque(4);
+
+			// Act & Assert
+			Assert.IsTrue(deque.InsertLast(1));
+			Assert.AreEqual(1, deque.GetFront());
+		}
+
+		[Test]
+		public void RearPointTheSameElementAsPointTest()
+		{
+			// Arrange
+			var deque = new CircularDeque(4);
+
+			// Act & Assert
+			Assert.IsTrue(deque.InsertFront(1));
+			Assert.AreEqual(1, deque.GetRear());
+		}
+
+		[Test]
+		public void FrontTest()
+		{
+			// Arrange
+			var deque = new CircularDeque(3);
+
+			// Act & Assert
+			Assert.IsTrue(deque.InsertFront(1));
+			Assert.IsTrue(deque.InsertFront(2));
+			Assert.AreEqual(2, deque.GetFront());
+			Assert.IsTrue(deque.InsertFront(3));
+			Assert.AreEqual(3, deque.GetFront());
+			Assert.IsTrue(deque.IsFull());
+			Assert.IsFalse(deque.InsertFront(4));
+			Assert.IsTrue(deque.DeleteFront());
+			Assert.IsFalse(deque.IsFull());
+			Assert.IsTrue(deque.InsertFront(4));
+			Assert.AreEqual(4, deque.GetFront());
+			Assert.IsTrue(deque.DeleteFront());
+			Assert.AreEqual(2, deque.GetFront());
+			Assert.IsTrue(deque.DeleteFront());
+			Assert.AreEqual(1, deque.GetFront());
+			Assert.IsTrue(deque.DeleteFront());
+			Assert.IsTrue(deque.IsEmpty());
+		}
+
+		[Test]
+		public void RearTest()
+		{
+			// Arrange
+			var deque = new CircularDeque(3);
+
+			// Act & Assert
+			Assert.IsTrue(deque.InsertLast(1));
+			Assert.IsTrue(deque.InsertLast(2));
+			Assert.AreEqual(2, deque.GetRear());
+			Assert.IsTrue(deque.InsertLast(3));
+			Assert.AreEqual(3, deque.GetRear());
+			Assert.IsTrue(deque.IsFull());
+			Assert.IsFalse(deque.InsertLast(4));
+			Assert.IsTrue(deque.DeleteLast());
+			Assert.IsFalse(deque.IsFull());
+			Assert.IsTrue(deque.InsertLast(4));
+			Assert.AreEqual(4, deque.GetRear());
+			Assert.IsTrue(deque.DeleteLast());
+			Assert.AreEqual(2, deque.GetRear());
+			Assert.IsTrue(deque.DeleteLast());
+			Assert.AreEqual(1, deque.GetRear());
+			Assert.IsTrue(deque.DeleteLast());
+			Assert.IsTrue(deque.IsEmpty());
+		}
+	}
+}
