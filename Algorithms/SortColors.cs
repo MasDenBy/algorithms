30b5e18c5795c0db90b52795e54849859a191e49
MasDenBy <horssupport@gmail.com>
2020-03-31 20:42:36 +0200

    Solve SortColors problem with merge sort

---
+++ Algorithms/SortColors.cs
@@ -0,0 +1,43 @@
+﻿namespace Algorithms
+{
+	public class SortColors
+	{
+		public void Sort(int[] nums)
+		{
+			int[] temp = new int[nums.Length];
+			nums.CopyTo(temp, 0);
+
+			Sort(temp, nums, 0, nums.Length - 1);
+		}
+
+		private void Sort(int[] temp, int[] nums, int low, int high)
+		{
+			if(high <= low)
+				return;
+
+			int mid = low + (high - low) / 2;
+
+			Sort(temp, nums, low, mid);
+			Sort(temp, nums, mid + 1, high);
+
+			Merge(temp, nums, low, mid, high);
+		}
+
+		private void Merge(int[] temp, int[] nums, int low, int middle, int high)
+		{
+			for (int i = low; i <= high; i++)
+				temp[i] = nums[i];
+
+			int left = low;
+			int right = middle + 1;
+
+			for (int index = low; index <= high; index++)
+			{
+				if (left > middle) nums[index] = temp[right++];
+				else if (right > high) nums[index] = temp[left++];
+				else if (temp[right] < temp[left]) nums[index] = temp[right++];
+				else nums[index] = temp[left++];
+			}
+		}
+	}
+}
---
+++ AlgorithmsTest/SortColorsTest.cs
@@ -0,0 +1,20 @@
+﻿using Algorithms;
+using NUnit.Framework;
+
+namespace AlgorithmsTest
+{
+	[TestFixture]
+	public class SortColorsTest
+	{
+		[TestCase(new int[] { 2, 0, 2, 1, 1, 0 }, new int[] { 0, 0, 1, 1, 2, 2 })]
+		[TestCase(new int[] { 2, 0, 2, 1, 1, 1, 0, 1 }, new int[] { 0, 0, 1, 1, 1, 1, 2, 2 })]
+		public void SortTest(int[] input, int[] output)
+		{
+			// Arrange & Act
+			new SortColors().Sort(input);
+
+			// Arrange
+			CollectionAssert.AreEqual(output, input);
+		}
+	}
+}
