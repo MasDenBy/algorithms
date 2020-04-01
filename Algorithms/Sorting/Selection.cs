54f4f95c8eb9c1b41d7def913a068165ea7b1814
MasDenBy <horssupport@gmail.com>
2020-04-01 15:10:26 +0200

    Selection sort implementation

---
+++ Algorithms/Sorting/Selection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+	public static class Selection
+	{
+		public static void Sort<T>(IList<T> values)
+			where T : IComparable
+		{
+			int min;
+			for (int i = 0; i < values.Count; i++)
+			{
+				min = i;
+
+				for (int j = i + 1; j < values.Count; j++)
+				{
+					if (values[min].CompareTo(values[j]) > 0)
+						min = j;
+				}
+
+				Swap(values, i, min);
+			}
+		}
+
+		private static void Swap<T>(IList<T> values, int from, int to) 
+			where T : IComparable
+		{
+			var temp = values[from];
+			values[from] = values[to];
+			values[to] = temp;
+		}
+	}
+}
---
+++ AlgorithmsTest/Sorting/SelectionTest.cs
@@ -0,0 +1,20 @@
+﻿using Algorithms.Sorting;
+using NUnit.Framework;
+
+namespace AlgorithmsTest.Sorting
+{
+	[TestFixture]
+	public class SelectionTest
+	{
+		[TestCase(new[] { 0 }, new[] { 0 })]
+		[TestCase(new[] { 0, 3, 2, 7, 10, 3, 3 }, new[] { 0, 2, 3, 3, 3, 7, 10 })]
+		public void SortTest(int[] input, int[] expected)
+		{
+			// Arrange & Act
+			Selection.Sort(input);
+
+			// Assert
+			CollectionAssert.AreEqual(expected, input);
+		}
+	}
+}
