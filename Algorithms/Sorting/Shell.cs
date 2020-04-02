76592c83b176b63c0e3b1b92bf5104aea193c443
MasDenBy <horssupport@gmail.com>
2020-04-02 16:26:40 +0200

    The implementation of Shellsort

---
+++ Algorithms/Sorting/Shell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+	public static class Shell
+	{
+		public static void Sort<T>(IList<T> values)
+			where T : IComparable
+		{
+			if (values.Count == 1)
+				return;
+
+			int h = 1;
+			while (h < values.Count / 3)
+				h = 3 * h + 1;
+
+			while (h >= 1)
+			{
+				for (int i = h; i < values.Count; i++)
+				{
+					for (int j = i; j >= h && values[j].LessThan(values[j-h]); j-=h)
+					{
+						values.Swap(j, j - h);
+					}
+				}
+
+				h = h / 3;
+			}
+		}
+	}
+}
