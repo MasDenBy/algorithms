cde52eda7a8d0143b76f8812244b33eac607c3df
MasDenBy <horssupport@gmail.com>
2020-04-10 15:34:39 +0200

    Implementation of Heap sort

---
+++ Algorithms/Sorting/Heap.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+	public static class Heap
+	{
+		public static void Sort<T>(IList<T> values)
+			where T : IComparable
+		{
+			if (values.Count == 1)
+				return;
+
+			int n = values.Count - 1;
+
+			// Fix each triangle to have max binary heap
+			for (int i = Parent(n); i >= 0; i--)
+				Sink(values, i, n);
+
+			// Move each max element to the end of the array and fix max binary heap
+			while (n > 0)
+			{
+				values.Swap(0, n--);
+				Sink(values, 0, n);
+			}
+		}
+
+		private static void Sink<T>(IList<T> values, int root, int last)
+			where T : IComparable
+		{
+			while (Left(root) <= last)
+			{
+				int left = Left(root);
+				int right = 2 * root + 2;
+
+				// if left element less than right element increaset left+1 to have 
+				// comparation with biggest element
+				if (left < last && values[left].LessThan(values[right]))
+					left++;
+
+				if (values[root].BiggerThan(values[left]))
+					break;
+
+				values.Swap(root, left);
+				root = left;
+			}
+		}
+
+		private static int Parent(int index) => index / 2 - 1;
+		private static int Left(int root) => 2 * root + 1;
+	}
+}
---
+++ AlgorithmsTest/Sorting/HeapTest.cs
@@ -0,0 +1,35 @@
+﻿using Algorithms.Sorting;
+using NUnit.Framework;
+
+namespace AlgorithmsTest.Sorting
+{
+	[TestFixture]
+	public class HeapTest
+	{
+		[Test]
+		public void SortIntTest()
+		{
+			// Arrange
+			var values = new[] { 3, 7, 2, 5, 9, 12, 34, 5, 8 };
+
+			// Act
+			Heap.Sort(values);
+
+			// Assert
+			CollectionAssert.AreEqual(new[] { 2, 3, 5, 5, 7, 8, 9, 12, 34 }, values);
+		}
+
+		[Test]
+		public void SortCharTest()
+		{
+			// Arrange
+			var values = new[] { 's', 'o', 'r', 't', 'e', 'x', 'a', 'm', 'p', 'l', 'e' };
+
+			// Act
+			Heap.Sort(values);
+
+			// Assert
+			CollectionAssert.AreEqual(new[] { 'a', 'e', 'e', 'l', 'm', 'o', 'p', 'r', 's', 't', 'x' }, values);
+		}
+	}
+}
