3c25cb3b197ba7b210b165da337a63ec3ecefb65
MasDenBy <horssupport@gmail.com>
2020-05-22 13:56:08 +0200

    LeetCode: 1309. Decrypt String from Alphabet to Integer Mapping

---
+++ LeetCode.Test/StringToIntegerMappingTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+	[TestFixture]
+	public class StringToIntegerMappingTest
+	{
+		[TestCase("10#11#12", "jkab")]
+		[TestCase("1326#", "acz")]
+		[TestCase("25#", "y")]
+		[TestCase("12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#", "abcdefghijklmnopqrstuvwxyz")]
+		public void FreqAlphabetsTest(string s, string expected)
+		{
+			// Act & Arrange
+			var result = new StringToIntegerMapping().FreqAlphabets(s);
+
+			// Assert
+			Assert.AreEqual(expected, result);
+		}
+	}
+}
---
+++ LeetCode/StringToIntegerMapping.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+	/// <summary>
+	/// 1309. Decrypt String from Alphabet to Integer Mapping
+	/// </summary>
+	public class StringToIntegerMapping
+	{
+		private static readonly string[] alphabet = new string[] {
+			"", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+			"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+		public string FreqAlphabets(string s)
+		{
+			var queue = new Queue<char>();
+			var sb = new StringBuilder();
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				var value = s[i];
+
+				if(value.Equals('#'))
+				{
+					ProcessQueue(queue, sb);
+				}
+				else
+				{
+					queue.Enqueue(value);
+				}
+			}
+
+			if (queue.Count > 0)
+			{
+				if(s.EndsWith("#"))
+				{
+					ProcessQueue(queue, sb);
+				}
+				else
+				{
+					while (queue.Count > 0)
+						sb.Append(GetLetter(queue.Dequeue().ToString()));
+				}
+			}
+
+			return sb.ToString();
+		}
+
+		private static void ProcessQueue(Queue<char> queue, StringBuilder sb)
+		{
+			while (queue.Count > 2)
+				sb.Append(GetLetter(queue.Dequeue().ToString()));
+
+			sb.Append(GetLetter($"{queue.Dequeue()}{queue.Dequeue()}"));
+		}
+
+		private static string GetLetter(string value)
+		{
+			var index = int.Parse(value);
+			return alphabet[index];
+		}
+	}
+}
