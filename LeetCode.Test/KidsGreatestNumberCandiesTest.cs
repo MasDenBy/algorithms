9f9c09c3fc7944eb441ab1b93a8be0f9c3dee46f
MasDenBy <horssupport@gmail.com>
2020-06-01 15:55:51 +0200

    Solved: Kids With the Greatest Number of Candies

---
+++ LeetCode.Test/KidsGreatestNumberCandiesTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+	[TestFixture]
+	public class KidsGreatestNumberCandiesTest
+	{
+		[TestCase(new int[] { 2, 3, 5, 1, 3 }, 3, new bool[] { true, true, true, false, true })]
+		public void KidsWithCandiesTest(int[] candies, int extraCandies, bool[] expected)
+		{
+			// Arrange & Act
+			var actual = new KidsGreatestNumberCandies().KidsWithCandies(candies, extraCandies);
+
+			// Assert
+			for (int i = 0; i < expected.Length; i++)
+				Assert.AreEqual(actual[i], expected[i]);
+		}
+	}
+}
---
+++ LeetCode/KidsGreatestNumberCandies.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// 1431. Kids With the Greatest Number of Candies
+    /// </summary>
+	public class KidsGreatestNumberCandies
+	{
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            int[] kidWithExtra = new int[candies.Length];
+            bool[] result = new bool[candies.Length];
+
+            for (int i = 0; i < candies.Length; i++)
+                kidWithExtra[i] = candies[i] + extraCandies;
+
+            for (int i = 0; i < candies.Length; i++)
+            {
+                var greatest = true;
+                for (int j = 0; j < candies.Length; j++)
+                {
+                    if (i == j) continue;
+                    greatest = kidWithExtra[i] >= candies[j];
+                    if (!greatest) break;
+                }
+                result[i] = greatest;
+            }
+
+            return result;
+        }
+    }
+}
