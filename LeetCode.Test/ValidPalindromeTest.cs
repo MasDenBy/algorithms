e6867e82cc1d168eb3d63579c76645931d9ba298
MasDenBy <horssupport@gmail.com>
2020-06-17 15:57:02 +0200

    LeetCode: 125. Valid Palindrome

---
+++ LeetCode.Test/ValidPalindromeTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+	[TestFixture]
+	public class ValidPalindromeTest
+	{
+		[TestCase("aa", true)]
+		[TestCase("", true)]
+		[TestCase("A man, a plan, a canal: Panama", true)]
+		[TestCase("!!!::vrTTr::v!!!", true)]
+		public void IsPalindromeTest(string s, bool expected)
+		{
+			Assert.AreEqual(expected, new ValidPalindrome().IsPalindrome(s));
+		}
+	}
+}
---
+++ LeetCode/ValidPalindrome.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode
+{
+    /// <summary>
+    /// 125. Valid Palindrome
+    /// </summary>
+	public class ValidPalindrome
+	{
+        public bool IsPalindrome(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) return true;
+
+            s = s.ToUpperInvariant();
+
+            int startIndex = 0;
+            int endIndex = s.Length - 1;
+
+            while(startIndex <= endIndex)
+            {
+                if (!char.IsLetterOrDigit(s[startIndex]))
+                {
+                    startIndex++;
+                    continue;
+                }
+
+                if (!char.IsLetterOrDigit(s[endIndex]))
+                {
+                    endIndex--;
+                    continue;
+                }
+
+                if (s[startIndex] == s[endIndex])
+                {
+                    startIndex++;
+                    endIndex--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
