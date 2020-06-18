e13d2f70eca570e70b00da9d510eb6c40bc67dfd
MasDenBy <horssupport@gmail.com>
2020-06-18 21:29:20 +0200

    LeetCode: 242 Valid anagram

---
+++ LeetCode.Test/ValidAnagramTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+	[TestFixture]
+	public class ValidAnagramTest
+	{
+		[TestCase("anagram", "nagaram", true)]
+		[TestCase("rat", "car", false)]
+		public void IsAnagramTest(string s, string t, bool expected)
+		{
+			// Act
+			Assert.AreEqual(expected, new ValidAnagram().IsAnagram(s, t));
+		}
+	}
+}
---
+++ LeetCode/ValidAnagram.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+	/// <summary>
+	/// 242. Valid Anagram
+	/// </summary>
+	public class ValidAnagram
+	{
+		public bool IsAnagram(string s, string t)
+		{
+			if (s.Length != t.Length) return false;
+
+			var sMap = new Dictionary<char, int>();
+			var tMap = new Dictionary<char, int>();
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				AddLetter(sMap, s[i]);
+				AddLetter(tMap, t[i]);
+			}
+
+			foreach (var map in sMap)
+			{
+				if (!tMap.ContainsKey(map.Key) || tMap[map.Key] != map.Value)
+					return false;
+			}
+
+			return true;
+		}
+
+		private static void AddLetter(Dictionary<char, int> dict, char letter)
+		{
+			dict[letter] = dict.ContainsKey(letter) ? dict[letter] + 1 : 1;
+		}
+	}
+}
