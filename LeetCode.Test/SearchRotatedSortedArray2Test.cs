a515a39db4ee9a18500da85f56c284b2b55c32e8
MasDenBy <horssupport@gmail.com>
2020-06-28 13:22:22 +0200

    LeetCode: Search in Rotated Sorted Array II

---
+++ LeetCode.Test/SearchRotatedSortedArray2Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+	[TestFixture]
+	public class SearchRotatedSortedArray2Test
+	{
+		[TestCase(new[] { 2, 5, 6, 0, 0, 1, 2 }, 0, true)]
+		[TestCase(new[] { 2, 5, 6, 0, 0, 1, 2 }, 3, false)]
+		[TestCase(new[] { 3, 1 }, 1, true)]
+		public void SearchTest(int[] nums, int target, bool expected)
+		{
+			Assert.AreEqual(expected, new SearchRotatedSortedArray2().Search(nums, target));
+		}
+	}
+}
---
+++ LeetCode/SearchRotatedSortedArray2.cs
@@ -0,0 +1,58 @@
+﻿using LeetCode.Entities;
+
+namespace LeetCode
+{
+	/// <summary>
+	/// 81. Search in Rotated Sorted Array II
+	/// </summary>
+	public class SearchRotatedSortedArray2
+	{
+		public bool Search(int[] nums, int target)
+		{
+			var bst = new BinarySearchTree();
+			bst.Build(nums);
+
+			return bst.Exists(target);
+		}
+
+		private class BinarySearchTree
+		{
+			private TreeNode root;
+
+			public void Build(int[] nums)
+			{
+				for (int i = 0; i < nums.Length; i++)
+					this.root = BuildNode(this.root, nums[i]);
+			}
+
+			public bool Exists(int number)
+			{
+				var x = this.root;
+
+				while (x != null)
+				{
+					var compResult = number.CompareTo(x.val);
+
+					if (compResult < 0) x = x.left;
+					else if (compResult > 0) x = x.right;
+					else return true;
+				}
+
+				return false;
+			}
+
+			private TreeNode BuildNode(TreeNode node, int value)
+			{
+				if (node == null) return new TreeNode(value);
+
+				var compResult = value.CompareTo(node.val);
+
+				if (compResult < 0) node.left = BuildNode(node.left, value);
+				else if (compResult > 0) node.right = BuildNode(node.right, value);
+				else node.val = value;
+
+				return node;
+			}
+		}
+	}
+}
